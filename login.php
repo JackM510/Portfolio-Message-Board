062b18ea850c884162d5829f9bf63cb1c7b155ee
JackM510 <nr@email.com>
2025-03-04 20:08:10 +1100

    Added more detail and styles to the login and sign up forms using bootstrap. Also made the forms switchable if the user does not have an account, they can click a div below the login form to create an account and vice versa.

--- login.php
+++ login.php
@@ -11,26 +11,67 @@
     
     <?php require_once "nav.php"; ?>
 
-    <div class="container">
-        <h2>Login</h2>
-        <form action="login.php" method="POST">
-            <input type="email" name="email" placeholder="Email" required>
-            <input type="password" name="password" placeholder="Password" required>
-            <button type="submit" name="login">Login</button>
-        </form>
+    <div class="container" id="login-form">
+        <div class="d-flex justify-content-center mt-3 mb-3">
+            <h2>Login</h2>
+        </div>
+        <div class="d-flex justify-content-center">
+            <form action="login.php" method="POST" class="w-50">
+                <div class="row">
+                    <div class="col-12 mb-3">
+                        <input class="form-control form-control-lg" type="email" name="email" placeholder="Email" required>
+                    </div>
+                    <div class="col-12 mb-3">
+                        <input class="form-control form-control-lg" type="password" name="password" placeholder="Password" required>
+                    </div>
+                    <div class="col-12 d-flex justify-content-center">
+                        <button class="btn btn-lg btn-primary" type="submit" name="login">Login</button>
+                    </div>
+                </div>
+            </form>
+        </div>
+        <div class="text-center mt-3" onclick="showSignUp()" style="cursor:pointer;">Don't have an account? Sign Up</div>
     </div>
     
-    <div class="container">
-        <h2>Sign Up</h2>
-        <form action="login.php" method="POST">
-            <input type="text" name="first_name" placeholder="First Name" required>
-            <input type="text" name="last_name" placeholder="Last Name" required>
-            <input type="email" name="email" placeholder="Email" required>
-            <input type="password" name="password" placeholder="Password" required>
-            <button type="submit" name="signup">Sign Up</button>
-        </form>
+    <div class="container" id="signup-form" style="display:none;">
+        <div class="d-flex justify-content-center mt-3 mb-3">
+            <h2>Sign Up</h2>
+        </div>
+        <div class="d-flex justify-content-center">
+            <form action="login.php" method="POST" class="w-50">
+                <div class="row">
+                    <div class="col-12 mb-3">
+                        <input class="form-control form-control-lg" type="text" name="first_name" placeholder="First Name" required>
+                    </div>
+                    <div class="col-12 mb-3">
+                        <input class="form-control form-control-lg" type="text" name="last_name" placeholder="Last Name" required>
+                    </div>
+                    <div class="col-12 mb-3">
+                        <input class="form-control form-control-lg" type="email" name="email" placeholder="Email" required>
+                    </div>
+                    <div class="col-12 mb-3">
+                        <input class="form-control form-control-lg" type="password" name="password" placeholder="Password" required>
+                    </div>
+                    <div class="col-12 d-flex justify-content-center">
+                        <button class="btn btn-lg btn-primary" type="submit" name="signup">Sign Up</button>
+                    </div>
+                </div>   
+            </form>
+        </div>
+        <div class="text-center mt-3" onclick="showLogin()" style="cursor:pointer;">Already have an account? Login</div>
     </div>
     
+    <script>
+        function showSignUp() {
+            document.getElementById("login-form").style.display = "none";
+            document.getElementById("signup-form").style.display = "block";
+        }
+
+        function showLogin() {
+            document.getElementById("login-form").style.display = "block";
+            document.getElementById("signup-form").style.display = "none";
+        }
+    </script>
 
 </body>
 </html>
