753a41391b8dda4b39bee43e7c7d007fee22f183
JackM510 <nr@email.com>
2025-03-04 18:26:56 +1100

    Added Bootstrap to head.php.

--- login.php
+++ login.php
@@ -4,28 +4,33 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <title>Message Board - Login</title>
+    <?php require_once "head.php" ?>
+    <title>Messageboard - Login</title>
 </head>
 <body>
     
-    <a href="index.php">Message Board</a></br>
+    <a href="index.php">Messageboard</a></br>
 
-    <h2>Login</h2>
-    <form action="login.php" method="POST">
-        <input type="email" name="email" placeholder="Email" required>
-        <input type="password" name="password" placeholder="Password" required>
-        <button type="submit" name="login">Login</button>
-    </form>
-
-    <h2>Sign Up</h2>
-    <form action="login.php" method="POST">
-        <input type="text" name="first_name" placeholder="First Name" required>
-        <input type="text" name="last_name" placeholder="Last Name" required>
-        <input type="email" name="email" placeholder="Email" required>
-        <input type="password" name="password" placeholder="Password" required>
-        <button type="submit" name="signup">Sign Up</button>
-    </form>
+    <div class="container">
+        <h2>Login</h2>
+        <form action="login.php" method="POST">
+            <input type="email" name="email" placeholder="Email" required>
+            <input type="password" name="password" placeholder="Password" required>
+            <button type="submit" name="login">Login</button>
+        </form>
+    </div>
+    
+    <div class="container">
+        <h2>Sign Up</h2>
+        <form action="login.php" method="POST">
+            <input type="text" name="first_name" placeholder="First Name" required>
+            <input type="text" name="last_name" placeholder="Last Name" required>
+            <input type="email" name="email" placeholder="Email" required>
+            <input type="password" name="password" placeholder="Password" required>
+            <button type="submit" name="signup">Sign Up</button>
+        </form>
+    </div>
+    
 
 </body>
 </html>
