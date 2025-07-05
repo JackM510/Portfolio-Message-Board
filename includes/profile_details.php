4cf3284a7458af3957ec7f2b20755cb48438f6f0
JackM510 <jackcmarshall@hotmail.com>
2025-07-05 10:26:21 +1000

    made some minor changes to profile details width on smaller devices - to review again shortly

--- includes/profile_details.php
+++ includes/profile_details.php
@@ -1,5 +1,5 @@
 <!-- Profile details container -->
-<div id="profile-details" class="w-75 m-auto">
+<div id="profile-details" class="m-auto">
     <!-- Profile picture -->
     <div id="profile-picture-container">
         <img id="profile-picture" class="rounded-pill" src="<?php echo htmlentities($profile_picture); ?>" alt="Profile Picture">
