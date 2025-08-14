ea03f8177c3a963306d75ede20a1597fb4488ff6
JackM510 <jackcmarshall@hotmail.com>
2025-08-14 19:06:58 +1000

    tracking all relevant files for utils.php push

---
+++ includes/utils/utilities.php
@@ -0,0 +1,10 @@
+<?php 
+    function isLoggedIn() {
+        if (!isset($_SESSION['user_id']) && !isset($_SESSION['email']) && !isset($_SESSION['first_name']) && !isset($_SESSION['role'])) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+?>
