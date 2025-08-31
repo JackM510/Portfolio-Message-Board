e527c0d7421c47769148709b1aa32ef40c9586a7
JackM510 <37175625+JackM510@users.noreply.github.com>
2025-08-31 10:05:13 +1000

    Update db_connection.php
    Updating password used for localhost connection

--- includes/db_connection.php
+++ includes/db_connection.php
@@ -1,7 +1,7 @@
 <?php
 try {
     // Simple PDO connection
-    $pdo = new PDO('mysql:host=localhost;dbname=messageboard', 'jackm', 'password3490');
+    $pdo = new PDO('mysql:host=localhost;dbname=messageboard', 'jackm', 'Zr8!vLx@29eQ#fT1');
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     // Catch connection errors
