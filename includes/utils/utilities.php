402cfec41d5afad373ab180aa59a10531a9acd7f
JackM510 <jackcmarshall@hotmail.com>
2025-09-04 12:06:37 +1000

    added ['profile_id'] to isLoggedIn() util check

--- includes/utils/utilities.php
+++ includes/utils/utilities.php
@@ -1,7 +1,7 @@
 <?php 
     // Check if a user is logged in
     function isLoggedIn() {
-        if (!isset($_SESSION['user_id']) && !isset($_SESSION['email']) && !isset($_SESSION['first_name']) && !isset($_SESSION['role'])) {
+        if (!isset($_SESSION['user_id']) && !isset($_SESSION['profile_id']) && !isset($_SESSION['email']) && !isset($_SESSION['first_name']) && !isset($_SESSION['role'])) {
             return false;
         } else {
             return true;
