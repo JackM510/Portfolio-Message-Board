0f7c9876d0273d09440cce0161585b82c69c85e1
JackM510 <jackcmarshall@hotmail.com>
2025-09-04 16:05:19 +1000

    cleaned up syntax and added comments to edit_comment.php

--- actions/edit_comment.php
+++ actions/edit_comment.php
@@ -2,19 +2,18 @@
 require_once __DIR__ . '/../config.php';
 session_start();
 require_once(DB_INC);
+require_once(UTIL_INC);
 
-if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_SESSION['user_id']) && isset($_POST['comment_id'])) {
+if ($_SERVER["REQUEST_METHOD"] == "POST" && isLoggedIn() && isset($_POST['comment_id'])) {
+    // Comment data
     $comment_id = $_POST['comment_id'];
-    $edited_comment = $_POST['edit_comment']; // Retrieve edited text
-
+    $edited_comment = $_POST['edit_comment']; 
     $edited_timestamp = date('Y-m-d H:i:s');
-
-
+    // UPDATE comment stmt
     $stmt = $pdo->prepare("UPDATE comments SET comment_text = :comment_text, comment_edited = :comment_edited WHERE comment_id = :comment_id");
     $stmt->bindParam(':comment_text', $edited_comment, PDO::PARAM_STR);
     $stmt->bindParam(':comment_edited', $edited_timestamp, PDO::PARAM_STR);
     $stmt->bindParam(':comment_id', $comment_id, PDO::PARAM_INT);
-
     if ($stmt->execute()) {
         echo "success";
     } else {
